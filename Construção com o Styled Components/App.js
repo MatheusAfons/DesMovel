6b6050839c1e380b2b8f6d2319f0a1c2e4306823
felipe <lipoliveira25@gmail.com>
2023-10-05 11:11:54 -0300

    Adicionado menu drawer

--- Construção com o Styled Components/App.js
+++ Construção com o Styled Components/App.js
@@ -2,9 +2,30 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faLock, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'; // Exemplo de ícones do FontAwesome
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
 import TextInput from './assets/TextInput';
 
-function App() {
+
+
+function Screen3({ navigation }) {
+  return (
+    <div>
+      tela3
+    </div>
+  );
+}
+
+function Screen2({ navigation }) {
+  return (
+    <div>
+      tela2
+    </div>
+  );
+}
+
+function LoginScreen ({ navigation }) {
   return (
     <div>
           <TextInput
@@ -18,6 +39,21 @@ function App() {
         placeholder="Senha"
       />
     </div>
+    
+  );
+}
+
+const Drawer = createDrawerNavigator()
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator useLegacyImplementation initialRouteName="Login">
+        <Drawer.Screen name="Login" component={LoginScreen} />
+        <Drawer.Screen name="Screen2" component={Screen2} />
+        <Drawer.Screen name="Screen3" component={Screen3} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
